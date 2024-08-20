37951d95b96a2d33aa3e5bfeec92879737472bed
Yifan Wang <62057231+asdpresence@users.noreply.github.com>
2024-08-20 21:54:16 +0800

    Create _init_.py

---
+++ osrl/algorithms/_init_.py
@@ -0,0 +1,6 @@
+from .bc import BC, BCTrainer
+from .bcql import BCQL, BCQLTrainer
+# from .bearl import BEARL, BEARLTrainer
+# from .cdt import CDT, CDTTrainer
+# from .coptidice import COptiDICE, COptiDICETrainer
+from .cpq import CPQ, CPQTrainer
